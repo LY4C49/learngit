d4ee75b812a87ee8e9f8db1ce49f6d4567ca0176
PeterLi <847565771@qq.com>
2023-10-15 21:39:12 -0700

    confilict?

--- hello.py
+++ hello.py
@@ -2,9 +2,12 @@ import numpy as np
 
 
 def test1():
-    print("hello word debugged")
+    print("hi")
+    print("hello worlllllllld debugged")
+    return 1
 
 
 
 def test2():
     print("test2")
+    return 2
